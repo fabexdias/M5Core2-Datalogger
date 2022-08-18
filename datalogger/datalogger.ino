47bae5980321c373a687110138e9978c24c98bda
Fábio Dias <fabiomagdias@hotmail.com>
2022-08-18 10:02:16 +0100

    Update datalogger.ino

--- datalogger/datalogger.ino
+++ datalogger/datalogger.ino
@@ -18,27 +18,13 @@ float Temps = 0, Tempi = 30;
 float PID_p = 0, PID_i = 0, PID_d = 0, PID_value = 0, PID_error = 0, PREV_error = 0;
 float K_p = 1.1, K_i = 0.5, K_d = 0.175;
 float Time_now = 0, Time_prev = 0, Time = 0;
-
-ButtonColors on_clrs  = {BLACK, BLACK, WHITE};
-ButtonColors off_clrs = {BLACK, WHITE, WHITE};
-Button A(220, 30, 80, 80, false ,"UP", off_clrs, on_clrs, MC_DATUM);
-Button B(220, 150, 80, 80, false ,"DOWN", off_clrs, on_clrs, MC_DATUM);
  
 RTC_TimeTypeDef RTCTime;
 RTC_DateTypeDef RTCDate;
- 
-void UpAndDown(Event& e) {
-  Button& b = *e.button;
-  if(b.y == 30){Tempi++;}
-  else if(b.y == 150){Tempi--;}
-  M5.Lcd.fillScreen(BLACK);
-  M5.Buttons.draw();
-}
 
 void Scroll(Event& e) {
   if(++selecting >= 6){selecting = 0;}
   M5.Lcd.fillScreen(BLACK);
-  M5.Buttons.draw();  
 } 
  
 int Limits(int Value, int SupLimit, int InfLimit){
@@ -47,7 +33,7 @@ int Limits(int Value, int SupLimit, int InfLimit){
   return Value;
 }
  
-void DownDate(Event& e) {
+void DateEvent(Event& e) {
   M5.Rtc.GetDate(&RTCDate);
   M5.Rtc.GetTime(&RTCTime);    
   int auxYear = RTCDate.Year;
@@ -57,86 +43,54 @@ void DownDate(Event& e) {
   int auxMinutes = RTCTime.Minutes;
   int auxSeconds = RTCTime.Seconds;
   M5.Lcd.fillScreen(BLACK);
-  M5.Buttons.draw();
+
+  if(M5.BtnC.wasPressed()){
+    auxYear = Limits(auxYear + 1, 1000000, 0);
+    auxMonth = Limits(auxMonth + 1, 12, 1);
+    auxDate = Limits(auxDate + 1, 31, 1);
+    auxHours = Limits(auxHours + 1, 23, 0);
+    auxMinutes = Limits(auxMinutes + 1, 59, 0);
+    auxSeconds = Limits(auxSeconds + 1, 59, 0);  
+  }else if(M5.BtnB.wasPressed()){
+    auxYear = Limits(auxYear - 1, 1000000, 0);
+    auxMonth = Limits(auxMonth - 1, 12, 1);
+    auxDate = Limits(auxDate - 1, 31, 1);
+    auxHours = Limits(auxHours - 1, 23, 0);
+    auxMinutes = Limits(auxMinutes - 1, 59, 0);
+    auxSeconds = Limits(auxSeconds - 1, 59, 0);    
+  }
 
   switch(selecting){
-    case 0:
-      auxYear = Limits(auxYear - 1, 1000000, 0);
+    case 3:
       RTCDate.Year = auxYear;
       break;
-    case 1:
-      auxMonth = Limits(auxMonth - 1, 12, 1);
+    case 4:
       RTCDate.Month = auxMonth;
       break;
-    case 2:
-      auxDate = Limits(auxDate - 1, 31, 1);
+    case 5:
       RTCDate.Date = auxDate;
-      break;//RTCDate.Year,RTCDate.Month,RTCDate.Date,RTCTime.Hours,RTCTime.Minutes,RTCTime.Seconds
-    case 3:
-      auxHours = Limits(auxHours - 1, 23, 0);
+      break;
+    case 0:
       RTCTime.Hours = auxHours;
       break;
-    case 4:
-      auxMinutes = Limits(auxMinutes - 1, 59, 0);
+    case 1:
       RTCTime.Minutes = auxMinutes;
       break;
-    case 5:
-      auxSeconds = Limits(auxSeconds - 1, 59, 0);
+    case 2:
       RTCTime.Seconds = auxSeconds;
       break;    
     default:
       break;
   }
+  
   M5.Rtc.SetDate(&RTCDate);
   M5.Rtc.SetTime(&RTCTime);
 }
- 
-void UpDate(Event& e) {
-  M5.Rtc.GetDate(&RTCDate);
-  M5.Rtc.GetTime(&RTCTime);  
-  int auxYear = RTCDate.Year;
-  int auxMonth = RTCDate.Month;
-  int auxHours = RTCTime.Hours;              
-  int auxDate = RTCDate.Date;
-  int auxMinutes = RTCTime.Minutes;
-  int auxSeconds = RTCTime.Seconds;
-  M5.Lcd.fillScreen(BLACK);
-  M5.Buttons.draw();    
- 
-  switch(selecting){
-    case 3:
-      auxYear = Limits(auxYear + 1, 1000000, 0);
-      RTCDate.Year = auxYear;
-      break;
-    case 4:
-      auxMonth = Limits(auxMonth + 1, 12, 1);
-      RTCDate.Month = auxMonth;
-      break;
-    case 5:
-      auxDate = Limits(auxDate + 1, 31, 1);
-      RTCDate.Date = auxDate;
-      break;
-    case 0:
-      auxHours = Limits(auxHours + 1, 23, 0);
-      RTCTime.Hours = auxHours;
-      break;
-    case 1:
-      auxMinutes = Limits(auxMinutes + 1, 59, 0);
-      RTCTime.Minutes = auxMinutes;
-      break;
-    case 2:
-      auxSeconds = Limits(auxSeconds + 1, 59, 0);
-      RTCTime.Seconds = auxSeconds;
-      break; 
-    default:
-      break;   
-  }
-  M5.Rtc.SetDate(&RTCDate);
-  M5.Rtc.SetTime(&RTCTime);
-} 
- 
+
 void setup(){
   M5.begin();
+  M5.Rtc.begin();
+  
   // Serial Config
   Serial2.begin(115200 , SERIAL_8N1, 32 , 33 );
   Serial2.setTimeout(300);
@@ -152,35 +106,22 @@ void setup(){
   // SDcard Config
   if (!SD.begin()){  
     str = "SDcard failed to mount.";
-    M5.Lcd.drawString(str, 20, 225, 2);
+    M5.Lcd.drawString(str, 0, 225, 2);
     sd_ok = false;
   }else{
     str = "SDcard successfully mounted.";
-    M5.Lcd.drawString(str, 20, 225, 2);
+    M5.Lcd.drawString(str, 0, 225, 2);
     sd_ok = true;
     do{
       file_name = "/file" + String(i) + ".txt";
       i++;
     }while(SD.exists(file_name));
   }
-
-  // RTC Config
-  RTCTime.Hours = 12;  
-  RTCTime.Minutes = 13;
-  RTCTime.Seconds = 20;
-  RTCDate.Year = 2022;
-  RTCDate.Month = 8;
-  RTCDate.Date = 11;
-  M5.Rtc.SetTime(&RTCTime);  
-  M5.Rtc.SetDate(&RTCDate);
-
+  
   // Button Config
   M5.BtnA.addHandler(Scroll, E_TOUCH);
-  M5.BtnB.addHandler(DownDate, E_TOUCH);
-  M5.BtnC.addHandler(UpDate, E_TOUCH);  
-  A.addHandler(UpAndDown, E_TOUCH);
-  B.addHandler(UpAndDown, E_TOUCH);  
-  M5.Buttons.draw();
+  M5.BtnB.addHandler(DateEvent, E_TOUCH);
+  M5.BtnC.addHandler(DateEvent, E_TOUCH);
 }
  
 void loop() {
@@ -214,10 +155,10 @@ void top_info(){
   Temps = ((1.1*analogRead(35)/4095*3.5481)-0.5)*100; // Using MCP9700 as temperature sensor
   str = "Read temp: ";
   str += String((int) round(Temps));
-  M5.Lcd.drawString(str, 20, 0, 4);
+  M5.Lcd.drawString(str, 0, 0, 4);
   str = "Ideal temp: ";
   str += String(Tempi);  
-  M5.Lcd.drawString(str, 300, 0, 4);
+  M5.Lcd.drawString(str, 0, 40, 4);
 
   M5.Rtc.GetDate(&RTCDate);
   M5.Rtc.GetTime(&RTCTime);
@@ -227,13 +168,18 @@ void top_info(){
   str += String(RTCTime.Minutes);
   str += "-";
   str += String(RTCTime.Seconds);
-  str += " Date: ";
+  M5.Lcd.drawString(str, 0, 80, 4); 
+  str = "Date: ";
   str += String(RTCDate.Year);      
   str += "-";
   str += String(RTCDate.Month);   
   str += "-";
   str += String(RTCDate.Date); 
-  M5.Lcd.drawString(str, 20, 25, 4); 
+  M5.Lcd.drawString(str, 0, 120, 4); 
+
+  if((RTCTime.Seconds == 0) || ((RTCTime.Seconds == 0) && (RTCTime.Minutes == 0)) || ((RTCTime.Seconds == 0) && (RTCTime.Minutes == 0) && (RTCTime.Hours == 0))){
+    M5.Lcd.fillScreen(BLACK);
+  }
 }
 
 void timed(){
